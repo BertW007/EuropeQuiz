a9a5f024c8544e488c8a9b6fb246c5a615e5b0f6
czarekp <cezarypiotrpietrzak@gmail.com>
2017-11-28 21:20:14 +0100

    Added onClickListeners for Buttons in MenuActivity

--- app/src/main/java/com/czarek/europequiz/MenuActivity.java
+++ app/src/main/java/com/czarek/europequiz/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.czarek.europequiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button play = findViewById(R.id.play_button);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent selectGameIntent = new Intent(MenuActivity.this, SelectGameActivity.class);
+
+                startActivity(selectGameIntent);
+            }
+        });
+
+        Button viewMap = findViewById(R.id.view_map_button);
+
+        viewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewMapIntent = new Intent(MenuActivity.this, InteractiveMapActivity.class);
+
+                startActivity(viewMapIntent);
+            }
+        });
+
     }
 }
